fae5393bd71ccf1cccb2395ffe0eb0a39d788a3c
NegevTK <luizantoniosoda@gmail.com>
2022-10-14 00:25:14 -0300

    alterações de tela inicial

--- mobilidade-lanchonete/lib/app/cardapio/app_cardapio_controller.dart
+++ mobilidade-lanchonete/lib/app/cardapio/app_cardapio_controller.dart
@@ -1,1 +1,4 @@
-class AppCardapioController {}
+class AppCardapioController {
+  String dropdownvalue = '1';
+  var mesas = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+}
--- mobilidade-lanchonete/lib/app/cardapio/app_cardapio_view.dart
+++ mobilidade-lanchonete/lib/app/cardapio/app_cardapio_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:lanchonete_faculdade/app/cardapio/app_cardapio_controller.dart';
 
 class AppCardapioView extends StatefulWidget {
   @override
@@ -6,8 +8,100 @@ class AppCardapioView extends StatefulWidget {
 }
 
 class _AppCardapioViewState extends State<AppCardapioView> {
+  final AppCardapioController _cardapioController =
+      Modular.get<AppCardapioController>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cardápio'),
+      ),
+      body: cardapio(),
+    );
+  }
+
+  Widget cardapio() {
+    return Column(
+      children: [
+        clienteMesa(),
+        listaLanches(),
+      ],
+    );
+  }
+
+  Widget clienteMesa() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        child: Row(children: [
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+                decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.person),
+              hintText: 'Cliente',
+              labelText: 'Cliente',
+            )),
+          )),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'MESA: ',
+                      style: TextStyle(
+                          fontWeight: FontWeight.normal, fontSize: 18.0),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DropdownButton(
+                      value: _cardapioController.dropdownvalue,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      items: _cardapioController.mesas.map((String items) {
+                        return DropdownMenuItem(
+                          value: items,
+                          child: Text(
+                            items,
+                            style: TextStyle(fontSize: 18.0),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String value) {
+                        setState(() {
+                          _cardapioController.dropdownvalue = value;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+
+  Widget listaLanches() {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(150)),
+            child: Image.asset(
+              'assets/lanche.jpg',
+            ),
+          );
+        },
+      ),
+    );
   }
 }
